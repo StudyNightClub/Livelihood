1d781e03405228c98de9462c0d06fd3708af5f6c
Lucas Wang <gtchiaming@gmail.com>
2017-06-13 22:26:24 +0800

    modify regex string

--- livelihood_database/location_parser.py
+++ livelihood_database/location_parser.py
@@ -3,26 +3,21 @@
 import re
 
 
+_ADDRESS_REGEX = '(?:台灣)?(.*?市)(.*?區)((?:.*?(?:路|街|大道|橋)(?:.*段)?)?)?(.*)'
+_DESCRIPTION_REGEX = '(?:.*時|.*分)(?:，)(.*)(?:，)'
+
+
 ### Address of POWER OUTAGE ###
 def parse_power_address(address_name):
-    regex = '(.*?市)(.*?區)((?:.*?路|.*?街|.*?大道|.*?橋)?(?:.*段)?)?(.*)?'
-    
-    sub_address = re.search(regex, address_name)
-    
-    #print(sub_address.groups()[0],sub_address.groups()[1], sub_address.groups()[2], sub_address.groups()[3])
-    
-    # sub_address.group(0) is address_name
-    #print(sub_address.group(1), sub_address.group(2), sub_address.group(3), sub_address.group(4))
+        
+    sub_address = re.search(_ADDRESS_REGEX, address_name)
     
     return sub_address.groups()
 
 ### Address of ROAD CONSTRUCTION ###
 def parse_road_address(address_name):
-    regex = '(.{2}?市)(.*?區)((?:.*?路|.*?街|.*?大道|.*?橋)?(?:.*段)?)?(.*)?'
     
-    sub_address = re.search(regex, address_name)
-    
-    #print(sub_address.groups())
+    sub_address = re.search(_ADDRESS_REGEX, address_name)
     
     return sub_address.groups()
 
@@ -30,20 +25,14 @@ def parse_road_address(address_name):
 def parse_water_address(address_name, field_name):
     
     if field_name == 'description':
-        regex = '(?:.*時|.*分)(?:，)(.*)(?:，)'
-        
-        sub_address = re.search(regex, address_name)
-        
-        #print(sub_address.groups())
+                
+        sub_address = re.search(_DESCRIPTION_REGEX, address_name)
         
         return sub_address.groups()
     
     elif field_name == 'location':
-        regex = '(.{2}?市)(.*?區)((?:.*?路|.*?街|.*?大道|.*?橋)?(?:.*段)?)?(.*)?'
         
-        sub_address = re.search(regex, address_name)
-        
-        #print(sub_address.groups())
+        sub_address = re.search(_ADDRESS_REGEX, address_name)
         
         return sub_address.groups()
     
